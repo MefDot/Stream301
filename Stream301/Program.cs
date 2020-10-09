1a335351bb6e85edd2edb98df6dbc2654d62e26e
Yury <66883095+MefDot@users.noreply.github.com>
2020-10-09 12:46:39 +0900

    Final

--- Stream301/Program.cs
+++ Stream301/Program.cs
@@ -8,10 +8,16 @@ namespace Stream301
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-
-
+            Table stol = new Table(30, 15, "красный", "дерево", "квадратная");
+            Console.WriteLine($"Цвет вашего стола {stol.ReturnColorTable()}");
+            Console.WriteLine($"Размеры вашего стола {stol.SquareTable()}");
+            Console.WriteLine("Введите новый цвет стола ");
+            stol.SetColor(Console.ReadLine());
+           Console.WriteLine($"New Цвет вашего стола {stol.ReturnColorTable()}");
+            Console.ReadLine();
 
         }
     }
---
+++ Stream301/Table.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream301
+{
+    class Table
+    {
+        double width;
+        double height;
+        string Color;
+        string Material;
+        string forma;
+
+        public Table(double width, double height, string color, string material, string forma)
+        {
+            this.width = width;
+            this.height = height;
+            Color = color;
+            
+            Material = material;
+            this.forma = forma;
+        }
+
+
+        public double SquareTable()
+        {
+            return width / height;
+        }
+
+        public string ReturnForm()
+        {
+            return forma;
+        }
+
+        public void SetColor(string color) /// Метод для установки цвета
+        {
+            Color = color;
+        }
+
+        public string ReturnColorTable() // Метод которы возвращает цвет
+        {
+            return Color;
+        }
+       
+    }
+}
